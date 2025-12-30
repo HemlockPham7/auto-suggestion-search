9a615ea3261c9f21cb6e34bb0b44d99206d4eb48
Viet-Hoang Pham <157110579+HemlockPham7@users.noreply.github.com>
2025-12-31 06:15:33 +0700

    query aggregation

---
+++ 11-playaround/autosuggestion/src/test/java/com/convit/autosuggestion/sec05/repository/GarmentRepository.java
@@ -0,0 +1,9 @@
+package com.convit.autosuggestion.sec05.repository;
+
+import com.convit.autosuggestion.sec05.entity.Garment;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GarmentRepository extends ElasticsearchRepository<Garment, String> {
+}
